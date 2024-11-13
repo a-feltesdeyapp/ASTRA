66ca22682c82195c3d71b7394152bfdbf46eaee0
auma4987 <103540065+auma4987@users.noreply.github.com>
2024-11-13 01:25:13 -0700

    Updated copCalc.py
    Updated to include a function for configuration in which pressure sensors are in the corner. Also added support for rectangular boards.

--- ControlCode/copCalc.py
+++ ControlCode/copCalc.py
@@ -1,6 +1,18 @@
-def copCalc(a,F1,F2,F3,F4):
-    # calculates center of pressure
-    # a contains the length from the center of the board to each pressure 
-    W=F1+F2
-    cop = [a*(F1-F3)/W,a*(F2-F4)/W]
+def copCalc(a,b,F1,F2,F3,F4):
+    # calculates center of pressure for pressure sensors in cardinal directions
+    # a contains the length from the center of the board to left and right sides
+    # b contains the length from the center of the board to forward and backwards sides
+
+    W1 = F1+F3
+    W2 = F2+F4
+    cop = [a*(F1-F3)/W1,a*(F2-F4)/W2]
+    return cop
     
+def copCornerCalc(a,b,F1,F2,F3,F4):
+    # calculates center of pressure for pressure sensors in corners
+    # a contains the length from the center of the board to left and right sides
+    # b contains the length from the center of the board to forward and backwards sides
+
+    W = F1+F2+F3+F4
+    cop = [a*((F1+F2)-(F3+F4))/W, b*((F1+F4)-(F2+F3))/W]
+    return cop
