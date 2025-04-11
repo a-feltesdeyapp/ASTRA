ef56792b423b0525b74a2fad0e15981af8c88295
auma4987 <103540065+auma4987@users.noreply.github.com>
2025-04-11 01:29:35 -0600

    Update cop_read.py
    Working CoP code!

--- COP Code/cop_read.py
+++ COP Code/cop_read.py
@@ -1,5 +1,6 @@
 # Meant to grab variables from console output from Wii Balance Walker and print them as a proof of concept
 import subprocess
+import sys
 process = subprocess.Popen(['./WiiBalanceWalker-0.5/WiiBalanceWalker-0.5/WiiBalanceWalker/bin/x64/Release/WiiBalanceWalker.exe'], 
                              stdin=subprocess.PIPE, 
                              stdout=subprocess.PIPE,
@@ -7,5 +8,5 @@ process = subprocess.Popen(['./WiiBalanceWalker-0.5/WiiBalanceWalker-0.5/WiiBala
                              text=True)
 
 while True:
-    output, errors = process.communicate()
+    output = process.stdout.readline()
     print(output)
